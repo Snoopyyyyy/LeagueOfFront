20cbe5bbc32d5d531122aa37c91a3819cb9b7488
Maelysami <maelys.ami@outlook.fr>
2022-09-28 10:34:30 +0200

    Edit style and environments

---
+++ LeagueOff/src/app/vue-content/vue-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vue-content',
+  templateUrl: './vue-content.component.html',
+  styleUrls: ['./vue-content.component.css']
+})
+export class VueContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
