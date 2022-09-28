fb891feeb268a41abac445d617e409278c0b7dcb
Snoopy <kentincosnard@gmail.com>
2022-09-28 14:43:34 +0200

    Correction rooting

--- LeagueOff/src/app/app-routing.module.ts
+++ LeagueOff/src/app/app-routing.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { VueContentComponent } from './vue-content/vue-content.component';
-
-
+import {HomePageComponent} from "./home-page/home-page.component";
 
 const routes: Routes = [
-  // Route de base (arrivée sur le site)
-  { path: '', component: AppComponent},
-  // Route vers un component particulier
+  { path: '', component: HomePageComponent},
   { path: 'vue-content', component: VueContentComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  
+
 })
 export class AppRoutingModule { }
--- LeagueOff/src/app/vue-content/vue-content.component.ts
+++ LeagueOff/src/app/vue-content/vue-content.component.ts
@@ -10,6 +10,7 @@ export class VueContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+	  console.log('call')
   }
 
 }
